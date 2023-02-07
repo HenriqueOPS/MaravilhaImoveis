3203762b103df8cac9ca45db622dbec0560c5ca9
HenriqueOPS <henriquepalmaifrs@gmail.com>
2023-02-07 09:49:37 -0300

    fix("tentativadetabelaCRUD")

--- Front-end/src/admin/index.js
+++ Front-end/src/admin/index.js
@@ -1,7 +1,34 @@
 import './admin.css' ;
 import React,{Component} from 'react';
-import api from '../api'
-import Tabela from './tabela';
+import api from '../api';
+import './tabela.css';
+function update(post){
+    console.log(post._id)
+}
+function Tabela(){
+    const [post, setPost] = React.useState(null);
+    React.useEffect(() => {
+        api.get('imovel').then((response) => {
+          setPost(response.data);
+        });
+      }, []);
+      if (!post) return null;
+      return(
+        <>
+        {post.map((post)=>(
+          <div className="row">
+            <input type='text' className="cel" placeholder={post.nome}></input>
+            <input type='text' className="cel" placeholder={post.metragem}></input>
+            <input type='text' className="cel" placeholder={post.localizacao}></input>
+            <input type='text' className="cel" placeholder={post.adicionais}></input>
+            <input type='text' className="cel" placeholder={post.preco}></input>
+            <div className="cel">{post._id}</div>
+            <button onClick={()=>{update(post)}}></button>
+          </div>
+        ))}
+        </>
+      )
+}
 class Admin extends Component{
     constructor(props){
     super(props);
